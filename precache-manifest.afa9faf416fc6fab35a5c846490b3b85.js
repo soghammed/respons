949a717fb30d38e46ff97f1ee24dcec4f08589b5
Mohammed hammed <devhammed@gmail.com>
2020-06-21 02:45:35 +0100

    minor update

---
+++ precache-manifest.afa9faf416fc6fab35a5c846490b3b85.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af71713b39d38a8b7d2f6e84d214ed2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f02057a03271c2951b95",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ccda8e9866f43b0f928f",
+    "url": "/static/css/main.5ff4cadb.chunk.css"
+  },
+  {
+    "revision": "f02057a03271c2951b95",
+    "url": "/static/js/2.93ae1184.chunk.js"
+  },
+  {
+    "revision": "777db246545e57363857b884a88741bf",
+    "url": "/static/js/2.93ae1184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccda8e9866f43b0f928f",
+    "url": "/static/js/main.bd808810.chunk.js"
+  },
+  {
+    "revision": "b288e7aab8e0f3f3fb9d",
+    "url": "/static/js/runtime-main.fccfcfd1.js"
+  },
+  {
+    "revision": "36fa85078bb61be1b5b54f9bad0ddf5d",
+    "url": "/static/media/bg.36fa8507.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
